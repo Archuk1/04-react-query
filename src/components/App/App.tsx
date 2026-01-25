b88e79659d7bd9c39da86b0dfb18d84520a5fa90
Vadym <vadimkiricenko572@gmail.com>
2026-01-25 21:21:39 +0200

    all done

---
+++ src/components/App/App.tsx
@@ -0,0 +1,62 @@
+import {useState } from "react"
+import SearchBar from "../SearchBar/SearchBar"
+import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
+import toast, { Toaster } from "react-hot-toast";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import css from "./App.module.css"
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
+
+
+
+function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
+  const handlSearch = async (newQuery: string)=>{
+    try{
+
+      setMovies([])
+      setLoading(true)
+      setIsError(false)
+      const data = await fetchMovies({query:newQuery});
+      if(data.results.length === 0){
+        toast.error(`No movies found for your request.`)
+      }
+
+      setMovies(data.results)
+
+    }catch{
+      setIsError(true)
+    }finally{
+      setLoading(false)
+    }
+
+  } 
+
+  const openModal = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsOpenModal(true)
+  }
+
+  const closeModal = () => {
+    setIsOpenModal(false)
+  }
+
+  return (<div className={css.app}>
+    <SearchBar onSubmit={handlSearch}/>
+    <Toaster/>
+    {loading && <Loader />}
+    {isError && <ErrorMessage/>}
+    {isOpenModal && selectedMovie && <MovieModal onClose={closeModal} movie={selectedMovie}/>}
+    {movies.length > 0 &&  <MovieGrid onSelect={openModal} movies={movies} />}
+    </div>
+  )
+}
+
+export default App
---
+++ src/services/movieService.ts
@@ -0,0 +1,44 @@
+import axios from "axios"
+import type { Movie } from "../types/movie"
+
+interface FetchMoviesParams{
+    page?:number,
+    language?:string,
+    include_adult?:boolean,
+    query:string 
+}
+
+interface FetchMoviesResponse{
+    page:number,
+    results: Movie[],
+    total_page:number,
+    total_results:number
+}
+
+
+const myKey = import.meta.env.VITE_TMDB_TOKEN;
+
+export async function fetchMovies ({page = 1,language = 'en-US' ,include_adult = false, query}: FetchMoviesParams): Promise<FetchMoviesResponse> {
+    try{const response = await axios({
+        method:'GET',
+        url:'https://api.themoviedb.org/3/search/movie',
+        params:{
+            page,
+            language,
+            include_adult,
+            query
+        },
+        headers:{
+            Authorization: `Bearer ${myKey}`,
+            accept: `application/json`
+        }
+    }) 
+
+    return response.data
+    }
+    catch (error){
+        console.error('Error fetching movies:', error);
+        throw error;
+    }
+}
+
